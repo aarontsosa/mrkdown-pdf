51efcc7944cfed71499d17193d3321f2112afffb
Aaron Sosa <aarontsosa@gmail.com>
2017-08-03 10:27:55 -0400

    Wrote the markdown conversion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const readline = require('readline')
+const markdownpdf = require('markdown-pdf')
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -14,9 +15,16 @@ rl.question('What is the file name? ', (filename) => {
             console.log(err.message)
             return;
         }
-        console.log('By God... I have FOUND IT!')
+        console.log('WE DID IT!... WEVE FOUND IT')
         let content = buffer.toString();
-        let upcased = content.toUpperCase();
-        console.log(upcased);
-    });
-});
+        markdownpdf().from.string(content)
+                 .to(filename + ".pdf", () =>{
+                    console.log("We've done it Marty!")
+                 })
+    //     console.log('By God... I have FOUND IT!')
+    //     let content = buffer.toString();
+    //     let upcased = content.toUpperCase();
+    //     console.log(upcased);
+    // });
+    })
+})
